83e4682e200657ec81dbb5015c435ce382d0c0d8
MishaSapsay <misha.sapsay@gmail.com>
2020-04-12 00:11:29 -0400

    Add Dice Rolling Page

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+
+function rollDice() {
+	var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 0 - 6
+var PlayerOneImages = "images/dice-six-faces-"+randomNumber1+".png";
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; // 0 - 6
+var PlayerTwoImages = "images/dice-six-faces-"+randomNumber2+".png";
+
+document.querySelector('.img1').setAttribute('src', PlayerOneImages);
+document.querySelector('.img2').setAttribute('src', PlayerTwoImages);
+	}	
